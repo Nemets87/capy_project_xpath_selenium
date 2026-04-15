6a2baed347a7ba64aa6c305963466761c9ff0198
Sergey_Fedorov <109182055+Nemets87@users.noreply.github.com>
2026-04-15 13:21:30 +0300

    Update test_buy_all_products.py
    123

--- tests/test_buy_all_products.py
+++ tests/test_buy_all_products.py
@@ -3,9 +3,11 @@ import pytest
 from pages.category_page import CatalogPage
 
 
-@allure.feature("Корзина")
+@allure.feature("Корзина проверка")
 @allure.story("Покупка товаров")
+@pytest.mark.order(3)
 class TestBuyAllProducts:
+      print("Start Test_3")
 
     @allure.title("Добавление всех товаров на странице в корзину")
     def test_add_all_products_to_cart(self, driver):
@@ -14,3 +16,4 @@ class TestBuyAllProducts:
         catalog_page.add_all_products_to_cart()
         cart_page = catalog_page.go_to_cart()
         cart_page.take_screenshot("cart_with_all_products")
+
